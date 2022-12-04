ea5dbca1724800edadfd2993b60d936b9fff247b
KXLAA <kolade.afode@yahoo.com>
2022-12-04 10:59:59 +0000

    feat: aoc day-4 typescript
    Signed-off-by: KXLAA <kolade.afode@yahoo.com>

---
+++ typescript/day-4.ts
@@ -0,0 +1,29 @@
+function getMaxPairs(input: any) {
+  const format: number[][][] = input
+    .trim()
+    .split(/\r?\n/)
+    .map((s: string) => s.split(","))
+    .map((d: string[]) => d.map((c) => c.split("-").map(Number)));
+
+  const overlap = format.filter((b: number[][]) => {
+    const first = b[0];
+    const second = b[1];
+
+    return (
+      (first[0] <= second[0] && first[1] >= second[1]) ||
+      (first[0] >= second[0] && first[1] <= second[1])
+    );
+  });
+
+  const total = format.filter((b: number[][]) => {
+    const first = b[0];
+    const second = b[1];
+
+    return first[1] >= second[0] && second[1] >= first[0];
+  });
+
+  return {
+    overlap: overlap.length,
+    total: total.length,
+  };
+}
